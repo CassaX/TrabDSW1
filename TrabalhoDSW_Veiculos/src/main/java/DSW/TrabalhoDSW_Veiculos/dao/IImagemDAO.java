a20021994c2c9648f11be4b8e933ae510948761f
OrDeP90 <pedroghiotto@estudante.ufscar.br>
2025-06-24 08:27:52 -0300

    update imagens

--- TrabalhoDSW_Veiculos/src/main/java/DSW/TrabalhoDSW_Veiculos/dao/IImagemDAO.java
+++ TrabalhoDSW_Veiculos/src/main/java/DSW/TrabalhoDSW_Veiculos/dao/IImagemDAO.java
@@ -1,10 +1,13 @@
 package DSW.TrabalhoDSW_Veiculos.dao;
 
-import DSW.TrabalhoDSW_Veiculos.domain.Imagem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface IImagemDAO extends JpaRepository<Imagem, Long> {
-    List<Imagem> findByVeiculoId(Long idVeiculo);
+import org.springframework.data.repository.CrudRepository;
+
+import DSW.TrabalhoDSW_Veiculos.domain.Imagem;
+import DSW.TrabalhoDSW_Veiculos.domain.Veiculo;
+
+public interface IImagemDAO extends CrudRepository<Imagem, Long> {
+    List<Imagem> findByVeiculo(Veiculo veiculo);
+    Imagem findById(long id);
 }
--- TrabalhoDSW_Veiculos/src/main/java/DSW/TrabalhoDSW_Veiculos/service/imp/ImagemService.java
+++ TrabalhoDSW_Veiculos/src/main/java/DSW/TrabalhoDSW_Veiculos/service/imp/ImagemService.java
@@ -1,40 +1,38 @@
-package DSW.TrabalhoDSW_Veiculos.service.imp;
-
-import DSW.TrabalhoDSW_Veiculos.dao.IImagemDAO; // Você precisará criar esta interface
-import DSW.TrabalhoDSW_Veiculos.domain.Imagem;
-import DSW.TrabalhoDSW_Veiculos.service.spec.IImagemService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+package DSW.TrabalhoDSW_Veiculos.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import DSW.TrabalhoDSW_Veiculos.dao.IImagemDAO;
+import DSW.TrabalhoDSW_Veiculos.domain.Imagem;
+import DSW.TrabalhoDSW_Veiculos.domain.Veiculo;
+import DSW.TrabalhoDSW_Veiculos.service.spec.IImagemService;
+
 @Service
-@Transactional(readOnly = false)
 public class ImagemService implements IImagemService {
 
     @Autowired
-    private IImagemDAO dao; // Injetar seu DAO/Repository para Imagem
+    private IImagemDAO dao;
 
     @Override
-    public void salvar(Imagem imagem) {
-        dao.save(imagem);
+    public Imagem salvar(Imagem imagem) {
+        return dao.save(imagem);
     }
 
     @Override
-    public void excluir(Long id) {
-        dao.deleteById(id);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
     public Imagem buscarPorId(Long id) {
         return dao.findById(id).orElse(null);
     }
 
     @Override
-    @Transactional(readOnly = true)
-    public List<Imagem> buscarPorVeiculo(Long idVeiculo) {
-        return dao.findByVeiculoId(idVeiculo); // Você precisará deste método em IImagemDAO
+    public List<Imagem> buscarPorVeiculo(Veiculo veiculo) {
+        return dao.findByVeiculo(veiculo);
+    }
+
+    @Override
+    public void excluir(Long id) {
+        dao.deleteById(id);
     }
 }
